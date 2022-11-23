6fef6b43e8d488998fd26bb660522804ee3dca6a
sprawdzone.it <88592160+sprawdzoneit@users.noreply.github.com>
2022-11-23 13:15:21 +0100

    Create firsttorun.ps1
    the script configures the basic settings of machines with the Windows Server operating system. Some of these settings have serious security implications: winrm and set-executionpolicy
    
    The script significantly shortened the preparation of our test environments and is an example for other users

---
+++ firsttorun.ps1
@@ -0,0 +1,37 @@
+#first to run sript
+#v 1.0
+#sprawdzone.it
+
+
+#rdp enable
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+#power profile set to High Performance
+powercfg.exe -SETACTIVE 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+
+#set time zone
+Set-TimeZone -id "Central European Standard Time"
+
+#network file & printer sharing enable
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True -Profile Any
+
+#enable winrm - attention! safety risk
+winrm quickconfig -quiet
+
+#set executionpolicy for scripts - attention! safety risk
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+
+#set network name
+$nname = (Get-NetAdapter -Name * -Physical).Name
+$lname = Read-Host -Prompt "Enter new network adapter name"
+Rename-NetAdapter $nname -NewName $lname
+
+
+#disable enhanced security in IE for admins
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}' -name "IsInstalled" -value 0
+
+
+#change hostname
+$cname = Read-Host -Prompt "Enter new hostname"
+Rename-Computer -NewName $cname -Restart
